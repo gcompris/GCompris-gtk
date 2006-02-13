4fa4c11692ee66a25c1416b03d81e87b5ffe503a
Bruno Coudoin <bcoudoin@src.gnome.org>
2006-02-13 23:16:34 +0000

    Updated, created screenshots of new activities.

--- docs/take_screenshots.sh
+++ docs/take_screenshots.sh
@@ -2,4 +2,4 @@
 # A command example I use to create the screenshots.
 # Move them in the screenshots directory with the name found
 # in their .xml.in menu name field
-xwd -name gcompris | convert - -quality 85 -resize 400x300 screenshot.jpg
+xwd -name gcompris | convert - -quality 85 -resize 400x300 $1.jpg
