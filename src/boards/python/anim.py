2e7cf90bd9a3bd63b5fd37235715c84dfb49f6bf
Bruno Coudoin <bcoudoin@src.gnome.org>
2007-06-20 21:38:20 +0000

    merged trunk 2697
    svn path=/branches/gcomprixo/; revision=2762

--- src/boards/python/anim.py
+++ src/boards/python/anim.py
@@ -823,7 +823,7 @@ class Gcompris_anim:
     # And finaly a STOP icon
     run = self.root_playingitem.add(
       gnomecanvas.CanvasPixbuf,
-      pixbuf = gcompris.utils.load_pixmap(gcompris.skin.image_to_skin("boardicons/draw.png")),
+      pixbuf = gcompris.utils.load_pixmap(gcompris.skin.image_to_skin("boardicons/draw.svg")),
       x = 16,
       y = 110,
       )
