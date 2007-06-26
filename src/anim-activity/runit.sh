066f2fc4ad4425247609c2d1be5c270fafa6af55
Bruno Coudoin <bcoudoin@src.gnome.org>
2007-06-26 11:48:44 +0000

    ported python anim activity to the new architecture
    svn path=/branches/gcomprixo/; revision=2806

---
+++ src/anim-activity/runit.sh
@@ -0,0 +1,12 @@
+activity=anim
+
+plugindir=.
+if [ -f .libs/lib$activity.so ]
+then
+  plugindir=.libs
+fi
+
+./gcompris.bin -L $plugindir -P . -A resources \
+    --config-dir=. -M activity \
+    --locale_dir=./locale -l /fun/$activity $*
+
