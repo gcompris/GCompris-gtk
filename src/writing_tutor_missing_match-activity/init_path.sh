8cf9f735adaf370842756d3e84505c6d7839f25e
Bruno Coudoin <bcoudoin@src.gnome.org>
2007-11-11 22:32:33 +0000

    uncompleted activity, it has never worked and I don't want to port it to goocanvas.
    svn path=/branches/gcomprixogoo/; revision=3133

--- src/writing_tutor_missing_match-activity/init_path.sh
+++
@@ -1,8 +0,0 @@
-# Set the different path for this activity
-# This is sourced by runit.sh
-path=$1
-activity=writing_tutor_missing_match
-plugindir=$path/../boards/.libs
-pythonplugindir=$path 
-resourcedir=$path/resources
-section="/experimental/writing_tutor"
