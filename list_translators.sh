732e32507e157f96a6653b97e5e6fb22616f8586
Bruno Coudoin <bcoudoin@src.gnome.org>
2008-03-12 21:24:19 +0000

    Updated our build system to support automake1.10 	You still need to copy config.rpath from the gettext 	package manualy. 	Merged from trunk r3296
    svn path=/branches/gcomprixogoo/; revision=3297

--- list_translators.sh
+++ list_translators.sh
@@ -2,14 +2,17 @@
 
 # usage: tranlators.sh LAST_RELEASE_TAG
 
+# Changed so that the list comes out sorted by .po file name
+# Mariano Suárez-Alvarez, Tue Feb 17 16:59:28 ART 2004
+
 if [ ! $# -eq 1 ] ; then
-	echo "usage: $0 LAST_RELEASE_TAG"
-	exit 1;
+        echo "usage: $0 LAST_RELEASE_TAG"
+        exit 1;
 fi
 
-cvs diff -r $1 po/ChangeLog | \
-  (awk  '/\.po/ { print gensub ("[:,]", "\n", "g", $3); }' | \
+svn diff -r $1 po/ChangeLog | \
+  (awk  '/\+.*[a-z][a-zA-Z@_]*\.po/ { print gensub ("[:,]", "\n", "g", $3); }' | \
     while read file; do
-  if [ -z "po/$file" ]; then continue; fi
-  echo $(grep "Last-Translator" po/$file | sed -e 's/"Last-Translator:  *\(.*\)  *<.*/\1/') "(${file%%.po})"
-done) | sort | uniq
+  if [ -z "$file" ]; then continue; fi
+  echo "(${file%%.po})" $(grep "Last-Translator" po/$file | sed -e 's/"Last-Translator:  *\(.*\)  *<.*/\1/')
+done) | sort | uniq | sed -e 's/\((.*)\) \(.*\)/\2 \1/'
