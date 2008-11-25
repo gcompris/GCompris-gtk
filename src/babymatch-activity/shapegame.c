acfe86322b3885074f6dd0e20d386a2446b98fb8
Bruno Coudoin <bcoudoin@src.gnome.org>
2008-11-25 00:24:51 +0000

    Fixed a crash in case of multiple sound list. This was especially the case
    	* src/boards/shapegame.c: (item_event_drag): Fixed a crash in case of multiple sound list.
    	  This was especially the case in the level 3 of the double entry activity.
              Merged from trunk Commit r3628
    	* src/canal_lock-activity/resources/canal_lock/canal_lock.svgz: Created by
    	  Stéphane Cabaraux tef/cyberground/org
    
    
    svn path=/branches/gcomprixogoo/; revision=3629

--- src/babymatch-activity/shapegame.c
+++ src/babymatch-activity/shapegame.c
@@ -940,16 +940,20 @@ item_event_drag(GooCanvasItem *item,
 	       * of sound rather than a single one */
 	      char *p = NULL;
 	      char *soundfile = g_strdup(shape->soundfile);
+	      char *soundfiles = soundfile;
 
-	      while ((p = strstr (soundfile, " ")))
+	      while ((p = strstr (soundfiles, " ")))
 		{
 		  *p='\0';
-		  gc_sound_play_ogg(soundfile, NULL);
-		  soundfile = p + 1;
-		  g_warning("soundfile = %s\n", soundfile);
+		  gc_sound_play_ogg(soundfiles, NULL);
+		  soundfiles = p + 1;
+		  g_warning("soundfile = %s\n", soundfiles);
 		}
 
-	      gc_sound_play_ogg(soundfile, NULL);
+	      if (soundfiles != soundfile)
+		gc_sound_play_ogg(soundfiles, NULL);
+	      else
+		gc_sound_play_ogg(soundfile, NULL);
 	      g_free(soundfile);
 	    }
 	  break;
