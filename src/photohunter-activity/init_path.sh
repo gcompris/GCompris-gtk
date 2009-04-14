f616807967aa0c591b9fbe600f7592bfe99f75a4
Bruno Coudoin <bcoudoin@src.gnome.org>
2009-04-14 21:16:13 +0000

    Created photo hunter activity by Marc Le Douarain
    svn path=/branches/gcomprixogoo/; revision=3807

---
+++ src/photohunter-activity/init_path.sh
@@ -0,0 +1,8 @@
+# Set the different path for this activity
+# This is sourced by runit.sh
+path=$1
+activity=photohunter
+plugindir=$path/.libs
+pythonplugindir=$path 
+resourcedir=$path/resources
+section="/puzzle"
