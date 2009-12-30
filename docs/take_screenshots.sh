2e604d5b508832ad62b694d61b771e82155e096b
Bruno Coudoin <bruno.coudoin@free.fr>
2009-12-30 19:47:32 +0100

    Adapted to new GCompris screen ratio.

--- docs/take_screenshots.sh
+++ docs/take_screenshots.sh
@@ -2,4 +2,4 @@
 # A command example I use to create the screenshots.
 # Move them in the screenshots directory with the name found
 # in their .xml.in menu name field
-xwd -name GCompris | convert - -quality 85 -resize 400x300 $1.jpg
+xwd -name GCompris | convert - -quality 85 -resize 50% $1.jpg
