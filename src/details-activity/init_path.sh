94fc8eb23b104b6b67015e0a44c833e3e6e2571d
Bruno Coudoin <bruno.coudoin@free.fr>
2010-06-23 00:32:13 +0200

    Added details activity by Marc Levivier.

---
+++ src/details-activity/init_path.sh
@@ -0,0 +1,8 @@
+# Set the different path for this activity
+# This is sourced by runit.sh
+path=$1
+activity=details
+plugindir=$path/../babymatch-activity/.libs
+pythonplugindir=$path 
+resourcedir=$path/resources
+section="/discovery/miscelaneous"
