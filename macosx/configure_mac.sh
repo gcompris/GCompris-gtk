ad99c7a016d35688bbef90b2d15b7f7524776801
Bruno Coudoin <bruno.coudoin@gcompris.net>
2011-03-05 12:19:26 +0100

    Fixed to have a proper python path to compile the MacOSX version.
    Now the generated MacOSX version have the python activities working.

--- macosx/configure_mac.sh
+++ macosx/configure_mac.sh
@@ -1,2 +1,4 @@
 cd ..
-./configure --prefix /Users/bruno/gtk/inst/ --libdir /Users/bruno/gtk/inst/lib --enable-shared --disable-static --enable-gnet --enable-py-build-only --with-python=/Users/bruno/gtk/inst/bin/python --enable-nsbundle --enable-sdlmixer
+export PYTHONPATH=/Users/bruno/gtk/inst/lib/python2.6/site-packages
+./configure --prefix /Users/bruno/gtk/inst/ --libdir /Users/bruno/gtk/inst/lib --enable-shared --disable-static --enable-gnet --enable-py-build-only --with-python=/Users/bruno/gtk/inst/bin/python --enable-nsbundle --enable-sdlmixer --disable-binreloc
+
