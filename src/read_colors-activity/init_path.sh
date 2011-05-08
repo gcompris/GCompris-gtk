cdd43cdd60d01fb657148dbffcc13fc4bbf56ed9
Bruno Coudoin <bruno.coudoin@free.fr>
2011-05-08 03:33:49 +0200

    advanced_color: remade this activity based on the new findit engine.

--- src/read_colors-activity/init_path.sh
+++
@@ -1,8 +0,0 @@
-# Set the different path for this activity
-# This is sourced by runit.sh
-path=$1
-activity=read_colors
-plugindir=$path/.libs
-pythonplugindir=$path 
-resourcedir=$path/resources
-section="/discovery/colors_group"
