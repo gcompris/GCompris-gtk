a1d1176c16dee33a6989ec86c972861e1cd57cf1
Bruno Coudoin <bruno.coudoin@free.fr>
2011-05-31 22:51:58 +0200

    instrument activity: new instrument activity by Sylvain Dechy (and reworked / integrated by Bruno.).

---
+++ src/instruments-activity/init_path.sh
@@ -0,0 +1,8 @@
+# Set the different path for this activity
+# This is sourced by runit.sh
+path=$1
+activity=instruments
+plugindir=$path/../boards/.libs
+pythonplugindir=$path/../findit-activity
+resourcedir=$path/resources
+section="/discovery/sound_group"
