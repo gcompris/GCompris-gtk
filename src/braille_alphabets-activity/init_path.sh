16d34c042e66892c2f4d630b7120cf2f3f93705b
Srishti Sethi <srishakatux@gmail.com>
2011-06-14 00:57:27 +0530

    Deleting all files for repushing

--- src/braille_alphabets-activity/init_path.sh
+++
@@ -1,8 +0,0 @@
-# Set the different path for this activity
-# This is sourced by runit.sh
-path=$1
-activity=braille_alphabets
-plugindir=$path/../boards/.libs
-pythonplugindir=$path 
-resourcedir=$path/resources
-section="/experimental"
