83005028dee6b2630a1baaead07759de9ab94bec
Bruno Coudoin <bruno.coudoin@free.fr>
2011-09-02 23:46:48 +0200

    braille object: we don't want this activity because it's not possible to make it international

--- src/braille_objects-activity/init_path.sh
+++
@@ -1,8 +0,0 @@
-# Set the different path for this activity
-# This is sourced by runit.sh
-path=$1
-activity=braille_objects
-plugindir=$path/../boards/.libs
-pythonplugindir=$path/../braille_alphabets-activity:$path/../braille_objects-activity
-resourcedir=$path/resources
-section="/experimental"
