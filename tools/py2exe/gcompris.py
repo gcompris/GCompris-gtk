a6b7a33fe9adc70dd7ea494b661f503862ddc4f0
Bruno Coudoin <bruno.coudoin@free.fr>
2011-12-27 18:02:24 +0100

    windows: updated the compilation chain for windows to compile against GTK 2.24

--- tools/py2exe/gcompris.py
+++ tools/py2exe/gcompris.py
@@ -23,6 +23,7 @@ import atk
 import gobject
 import uuid
 import ConfigParser
-from sqlite3 import dbapi2 as sqlite 
+import gio
+from sqlite3 import dbapi2 as sqlite
 
 print "Hello gcompris world now"
