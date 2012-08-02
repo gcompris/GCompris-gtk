33cb764acfbcfc82b8a2f1c16bbd5805b820ac84
Bruno Coudoin <bruno.coudoin@free.fr>
2012-08-02 23:17:26 +0200

    fixed bug #593656, removed the server feature.
    some gnet left was removed.

--- macosx/configure_mac.sh
+++ macosx/configure_mac.sh
@@ -1,4 +1,4 @@
 cd ..
 export PYTHONPATH=$HOME/gtk/inst/lib/python2.7/site-packages
-./configure --prefix $HOME/gtk/inst/ --libdir $HOME/gtk/inst/lib --enable-shared --disable-static --enable-gnet --enable-py-build-only --with-python=$HOME/gtk/inst/bin/python --enable-nsbundle --disable-binreloc
+./configure --prefix $HOME/gtk/inst/ --libdir $HOME/gtk/inst/lib --enable-shared --disable-static --enable-py-build-only --with-python=$HOME/gtk/inst/bin/python --enable-nsbundle --disable-binreloc
 
