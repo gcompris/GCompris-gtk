87df2c0f131a03b7b26de580f387479adbfeb2bb
Peter Albrecht <pa-dev@gmx.de>
2012-09-22 10:06:30 +0200

    mining: move tool classes to separate python file
    Since we need the BlockingArea also in mining_tutorial.py, we have to
    move it to a separate python file.
    Since the Area class also is kind of a general helper class, too, we
    also move it to the external class.

--- src/mining-activity/mining.py
+++ src/mining-activity/mining.py
@@ -32,6 +32,8 @@ import pango
 import random
 import cairo
 
+from mining_tools import Area, BlockingArea
+
 from gcompris import gcompris_gettext as _
 
 class Gcompris_mining:
@@ -1078,34 +1080,3 @@ class Sparkling:
   def __is_animation_playing(self):
     """ Tells us, if there is an animation running at the moment """
     return self.timer != None
-
-
-class Area:
-  """ A class for comfortable working with goocanvas.Bounds """
-
-  def __init__(self, bounds):
-    self.x1 = bounds.x1
-    self.x2 = bounds.x2
-    self.y1 = bounds.y1
-    self.y2 = bounds.y2
-
-    self.width = self.x2 - self.x1
-    self.height = self.y2 - self.y1
-
-    self.center_x = self.x1 + self.width / 2.0
-    self.center_y = self.y1 + self.height / 2.0
-
-
-class BlockingArea:
-  """ This class defines a blocking area, where no nugget should be put """
-
-  def __init__(self, x1, y1, x2, y2):
-    """
-    Constructor:
-      x1, y1, x2, y2 : bounding box of the blocker in canvas coordinates (0 - 800 / 0 - 520)
-    """
-    self.bounds = goocanvas.Bounds(x1, y1, x2, y2)
-
-  def get_bounds(self):
-    """ Return the bounds, defined in the constructor """
-    return self.bounds
