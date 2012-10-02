f475d35a92b092804265fc1b4bb195d841cb3d66
Bruno Coudoin <bruno.coudoin@free.fr>
2012-10-02 23:50:48 +0200

    color_mix_light-activity fixed init path.
    Can run it with ./runit.sh again.

--- src/color_mix_light-activity/init_path.sh
+++ src/color_mix_light-activity/init_path.sh
@@ -5,5 +5,5 @@ activity=color_mix_light
 plugindir=$path/../boards/.libs
 pythonplugindir=$path/../color_mix-activity:$path/../color_mix_light-activity
 resourcedir=$path/../color_mix-activity/resources
-section="/experimental"
+section="/discovery/colors_group"
 
