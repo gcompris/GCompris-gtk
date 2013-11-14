10bbcf4db90fe092ba7b1a59021a3c39495379ae
Bruno Coudoin <bcoudoin@gcompris.net>
2013-11-14 02:23:32 +0100

    lang, fixed 'back to last level' mode.
    fixed call to bonus so that we properly log
    success to each exercise.

--- src/lang-activity/lang.py
+++ src/lang-activity/lang.py
@@ -230,13 +230,13 @@ dialogue to\nenable the sound."), None)
       self.set_level(self.gcomprisBoard.level)
 
   def set_level(self, level):
-    # We are not yet started
-    if self.board_paused:
-      return
-
     self.gcomprisBoard.level = level;
     self.gcomprisBoard.sublevel = 1;
     gcompris.bar_set_level(self.gcomprisBoard)
+
+    # We are not yet started
+    if self.board_paused:
+      return
 
     if self.currentExercise:
       self.currentExercise.stop()
